4bd0e2fb13564455073aca36868b0071cb34a416
YerzhanBazargali <47858555+YerzhanBazargali@users.noreply.github.com>
2024-03-19 23:40:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var lang = document.getElementsByTagName('html')[0].getAttribute('lang');
-    console.log(lang);
-    fetch('translations/' + lang + '.json')
-        .then(response => response.json())
-        .then(data => {
-            document.querySelectorAll('[data-translate]').forEach(element => {
-                var key = element.getAttribute('data-translate');
-                if (data[key]) {
-                    element.textContent = data[key];
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Error loading translations:', error);
-        });
+    var lang = localStorage.getItem('lang') ? ;
+    document.getElementsByTagName('html')[0].setAttribute('lang', lang ? lang : 'kk');
 });
