0054e0216a1232444d2b766c3663d862a1433766
YerzhanBazargali <47858555+YerzhanBazargali@users.noreply.github.com>
2024-03-19 23:47:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     var lang = localStorage.getItem('lang') ;
     document.getElementsByTagName('html')[0].setAttribute('lang', lang ? lang : 'kk');
     console.log(localStorage.getItem('lang'));
+    langUpdate();
 });
